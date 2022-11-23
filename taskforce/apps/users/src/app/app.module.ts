cbbb652cc4af5ae91d5368488c675f41d4fc1899
ilya53nov <ilya53nov@yandex.ru>
2022-11-23 23:11:43 +0300

    Добавлен импорт модулей - авторизация, пользователи задачи

--- taskforce/apps/users/src/app/app.module.ts
+++ taskforce/apps/users/src/app/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TaskUserModule } from './task-user/task-user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [TaskUserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
