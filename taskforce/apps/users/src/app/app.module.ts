b42572c80668139434a70ee14d62b754d2cb49cd
ilya53nov <ilya53nov@yandex.ru>
2022-11-30 22:44:32 +0300

    В импорты добавлен модуль конфигурации

--- taskforce/apps/users/src/app/app.module.ts
+++ taskforce/apps/users/src/app/app.module.ts
@@ -1,12 +1,30 @@
 import { Module } from '@nestjs/common';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskUserModule } from './task-user/task-user.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import databaseConfig from '../config/database.config';
+import { USER_ENV_FILE_PATH } from './app.constant';
+import { validateEnvironments } from './env.validation';
+import { MongooseModule } from '@nestjs/mongoose';
+import { getMongoDbConfig } from '../config/mongodb.config';
 
 @Module({
-  imports: [TaskUserModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+      envFilePath: USER_ENV_FILE_PATH,
+      load: [databaseConfig],
+      validate: validateEnvironments,
+    }),
+    MongooseModule.forRootAsync(
+      getMongoDbConfig()
+    ),
+    TaskUserModule,
+    AuthModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
