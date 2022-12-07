90aaf321ff37b972c55f2bbb4ffdd13271fbb333
ilya53nov <ilya53nov@yandex.ru>
2022-12-08 00:20:54 +0300

    Добавлена prisma и модуль с категориями

--- taskforce/apps/tasks/src/app/app.module.ts
+++ taskforce/apps/tasks/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
 import { CommentsModule } from './comments/comments.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [TaskModule, CommentsModule],
+  imports: [TaskModule, CommentsModule, PrismaModule, CategoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
