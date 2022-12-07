112df0c3083761b5336d9bbb3a8f7eb0fa280056
ilya53nov <ilya53nov@yandex.ru>
2022-12-08 00:17:17 +0300

    Добавлены конфигурационные файлы prisma для работы с Postgres

---
+++ taskforce/apps/tasks/prisma/migrations/20221207202331_initial/migration.sql
@@ -0,0 +1,44 @@
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" TEXT NOT NULL,
+    "header" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "categoryTitle" TEXT NOT NULL,
+    "cost" INTEGER,
+    "dateExecutionAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "image" TEXT,
+    "address" TEXT,
+    "tags" TEXT[],
+    "authorId" TEXT NOT NULL,
+    "executerId" TEXT,
+    "status" TEXT,
+    "reactions" TEXT[],
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "title" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Comment" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "text" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "taskId" TEXT NOT NULL,
+
+    CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_title_key" ON "Category"("title");
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_categoryTitle_fkey" FOREIGN KEY ("categoryTitle") REFERENCES "Category"("title") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_taskId_fkey" FOREIGN KEY ("taskId") REFERENCES "Task"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
