7ab0be66824b7b7063d1e7b92d2351013f38f1d8
ilya53nov <ilya53nov@yandex.ru>
2022-12-07 23:54:01 +0300

    Рефакторинг

--- taskforce/libs/shared-types/src/lib/task.interface.ts
+++ taskforce/libs/shared-types/src/lib/task.interface.ts
@@ -1,11 +1,20 @@
+import { Category } from './category.interface';
+import { Comment } from './comment.interface';
+
 export interface Task {
-  _id?: string;
+  id?: string;
   header: string;
   description: string;
-  category: string;
+  category?: Category;
   cost?: number;
-  dateExecution?: Date;
+  dateExecutionAt?: Date;
   image?: string;
   address?: string;
-  tags: string[];
+  tags?: string[];
+  createdAt?: Date;
+  authorId: string;
+  executerId?: string;
+  comments?: Comment[];
+  reactions?: string[];
+  status?: string;  
 }
