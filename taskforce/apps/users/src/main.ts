f9eda05386e0526d49289eec6a245973c29e0db8
ilya53nov <ilya53nov@yandex.ru>
2022-12-14 22:06:44 +0300

    Зарегистрирован пайп валидации

--- taskforce/apps/users/src/main.ts
+++ taskforce/apps/users/src/main.ts
@@ -3,7 +3,7 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
@@ -22,7 +22,9 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('spec', app, document)
+  SwaggerModule.setup('spec', app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
   
   const port = process.env.PORT || 3334;
   await app.listen(port);
